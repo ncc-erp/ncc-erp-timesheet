284d8742ed9c19dd9b560f5782e3560c2cf7d8af
duongtranduc1402 <duong.tranduc@ncc.asia>
2023-09-27 11:36:10 +0700

    feature/52128 + BE create API manage user project for branch

---
+++ aspnet-core/src/Timesheet.Application/APIs/ProjectManagementBranchDirectors/ManageUserProjectForBranchs/Dto/GetAllValueOfUserInProjectByUserIdDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Timesheet.Entities;
+
+namespace Timesheet.APIs.ProjectManagementBranchDirectors.ManageUserProjectForBranchs.Dto
+{
+    public class GetAllValueOfUserInProjectByUserIdDto
+    {
+        public long ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectCode { get; set; }
+        public ValueOfUserType ValueOfUserType { get; set; }
+        public float ShadowPercentage { get; set; }
+        public float WorkingHours { get; set; }
+    }
+}
