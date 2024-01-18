e45067d2a4c920c58d75aa13ee6200264d99d482
pvt121001 <phanthanh12102001@gmail.com>
2024-01-18 10:20:24 +0700

    bugfix/54628 = fix expand menu cannot scroll down, Sort items in Management

--- angular/src/app/layout/sidebar-nav.component.ts
+++ angular/src/app/layout/sidebar-nav.component.ts
@@ -134,7 +134,7 @@ export class SideBarNavComponent extends AppComponentBase {
                 new MenuItem(
                     this.l("Team working calendar"),
                     "AbsenceDayOfTeam",
-                    "groups",
+                    "groups",   
                     "/app/main/off-day-project-for-user"
                 ),
                 new MenuItem(
@@ -145,6 +145,48 @@ export class SideBarNavComponent extends AppComponentBase {
                 ),
             ]),
             new MenuItem(this.l("Management"), "", "group_work", "", [
+                new MenuItem(
+                    this.l("Manage off/remote/onsite requests"),
+                    "AbsenceDayByProject",
+                    "rule",
+                    "/app/main/off-day-project"
+                ),
+                new MenuItem(
+                    this.l("Timesheet management"),
+                    "Timesheet",
+                    "date_range",
+                    "/app/main/timesheets"
+                ),
+                new MenuItem(
+                    this.l("Timesheets monitoring"),
+                    "TimesheetSupervision",
+                    "supervised_user_circle",
+                    "/app/main/timesheets-supervisior"
+                ),
+                new MenuItem(
+                    this.l("Project management"),
+                    "Project",
+                    "assessment",
+                    "/app/main/projects"
+                ),
+                new MenuItem(
+                    this.l("Review Interns"),
+                    "ReviewIntern",
+                    "rate_review",
+                    "/app/main/review"
+                ),
+                new MenuItem(
+                    this.l("Retrospectives"),
+                    "Retro",
+                    "event_note",
+                    "/app/main/retro"
+                ),
+                new MenuItem(
+                    this.l("Manage employee working times"),
+                    "ManageWorkingTime",
+                    "access_time",
+                    "/app/main/manage-working-times"
+                ),
                 new MenuItem(this.l("Team building"), "TeamBuilding", "store", "", [
                     new MenuItem(
                         this.l("Team building HR"),
@@ -203,54 +245,14 @@ export class SideBarNavComponent extends AppComponentBase {
                         "/app/main/komu-tracker"
                     ),
                 ]),
-                new MenuItem(
-                    this.l("Manage off/remote/onsite requests"),
-                    "AbsenceDayByProject",
-                    "rule",
-                    "/app/main/off-day-project"
-                ),
-                new MenuItem(
-                    this.l("Timesheet management"),
-                    "Timesheet",
-                    "date_range",
-                    "/app/main/timesheets"
-                ),
-                new MenuItem(
-                    this.l("Timesheets monitoring"),
-                    "TimesheetSupervision",
-                    "supervised_user_circle",
-                    "/app/main/timesheets-supervisior"
-                ),
-                new MenuItem(
-                    this.l("Project management"),
-                    "Project",
-                    "assessment",
-                    "/app/main/projects"
-                ),
-                new MenuItem(
-                    this.l("Manage employee working times"),
-                    "ManageWorkingTime",
-                    "access_time",
-                    "/app/main/manage-working-times"
-                ),
                 // new MenuItem(
                 //     this.l("Branch Manager"),
                 //     "ProjectManagementBranchDirectors",
                 //     "location_city",
                 //     "/app/main/branch-manager"
                 // ),
-                new MenuItem(
-                    this.l("Retrospectives"),
-                    "Retro",
-                    "event_note",
-                    "/app/main/retro"
-                ),
-                new MenuItem(
-                    this.l("Review Interns"),
-                    "ReviewIntern",
-                    "rate_review",
-                    "/app/main/review"
-                ),
+                
+                
             ]),
         ];
     }
@@ -260,7 +262,7 @@ export class SideBarNavComponent extends AppComponentBase {
     }
 
     ngOnInit(): void {
-        this.menuItems = this.getMenuItems();
+       this.menuItems = this.getMenuItems();
         this.patchMenuItems(this.menuItems);
         this.routerEvents
             .pipe(filter((event) => event instanceof NavigationEnd))
@@ -334,11 +336,7 @@ export class SideBarNavComponent extends AppComponentBase {
         }
     }
 
-    isMenuItemVisible(item: MenuItem): boolean {
-        if (!item.permissionName) {
-            return true;
-        }
-
+    isMenuItemVisible(item: MenuItem): boolean { 
         if (this.permission.isGranted(item.permissionName)) {
             return true;
         }
