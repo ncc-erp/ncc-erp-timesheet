2215b38773672454e7b8463679db03fb5d95d4b0
huy.tranngoc <huy.tranngoc@ncc.asia>
2024-08-21 13:48:36 +0700

    Close #270 -Branch Manager > add column level (sortable)

--- angular/src/app/modules/branch-manager/manage-employee/enum/sort-project-user-number.enum.ts
+++ angular/src/app/modules/branch-manager/manage-employee/enum/sort-project-user-number.enum.ts
@@ -3,9 +3,12 @@ export enum ESortProjectUserNumber {
     DOWN_PROJECT = 1,
     UP_NUMBER = 2,
     DOWN_NUMBER = 3,
+    UP_LEVEL = 4,
+    DOWN_LEVEL = 5,
 }
 
 export enum ESortType {
     PROJECT = 0,
     NUMBER = 1,
+    LEVEL = 2,
 }
