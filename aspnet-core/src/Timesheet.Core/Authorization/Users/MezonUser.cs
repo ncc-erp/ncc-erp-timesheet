f9ccdd8e7daea242ec924930f211af58d18ba67b
trunght <trung.hoangthanh@ncc.asia>
2025-03-19 17:45:45 +0700

    feat/implement-channel-app: add API hashmezon

---
+++ aspnet-core/src/Timesheet.Core/Authorization/Users/MezonUser.cs
@@ -0,0 +1,12 @@
+﻿namespace Timesheet.Authorization.Users
+{
+    public class MezonUser
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string display_name { get; set; }
+        public string avatar_url { get; set; }
+        public string mezon_id { get; set; }
+        public string sub { get; set; }
+    }
+}
