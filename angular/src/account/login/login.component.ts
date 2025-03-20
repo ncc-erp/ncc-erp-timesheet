f5712f1c4e5b5f02239eedd28002c8c5b6229079
trung-hoangthanh-ncc <trung.hoangthanh@ncc.asia>
2025-03-20 11:41:43 +0700

    Fix commit mistake

--- angular/src/account/login/login.component.ts
+++ angular/src/account/login/login.component.ts
@@ -7,9 +7,7 @@ import { AuthService, SocialUser } from "angularx-social-login";
 import { GoogleLoginProvider } from "angularx-social-login";
 import { AppConsts } from '@shared/AppConsts';
 import { MezonLoginService } from '@app/service/api/mezon-api.service';
-import { IHashMezonAuthModel } from '@shared/service-proxies/service-proxies';
-import { Base64 } from '@node_modules/js-base64/base64';
-import { AppAuthService } from '@shared/auth/app-auth.service';
+
 
 @Component({
   templateUrl: './login.component.html',
@@ -24,11 +22,7 @@ export class LoginComponent extends AppComponentBase implements OnInit {
 
   hashData: string;
   isMezonApp: boolean = false;
-  isAuthenFailed: boolean = false;
-  isAuthenticating: boolean = false;
-
-
-
+  
   constructor(
     injector: Injector,
     public loginService: LoginService,
--- angular/src/patches/abp.patch.js
+++ angular/src/patches/abp.patch.js
@@ -1,17 +1,3 @@
-// const originalSetCookieValue = abp.utils.setCookieValue;
-
-// abp.utils.setCookieValue = function (key, value, expireDate, path, domain, attributes) {
-//     var isEmbeddedApp = window.self !== window.top;
-
-//     attributes = attributes || {};
-
-//     if (isEmbeddedApp) {
-//         attributes['SameSite'] = 'None';
-//         attributes['Secure'] = true;
-//     }
-
-//     originalSetCookieValue.call(this, key, value, expireDate, path, domain, attributes);
-// };
 
 abp.auth.setToken = function (authToken, expireDate) {
     localStorage.setItem(abp.auth.tokenCookieName, authToken);
