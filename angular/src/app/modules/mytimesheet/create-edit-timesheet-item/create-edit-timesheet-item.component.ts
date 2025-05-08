f278294c5597a40e1823ed506bed350505a87822
Tên GitHub Mới <Email GitHub Mi>
2025-05-08 17:33:00 +0700

    feat: default Open Talk = 4h and allow edit workingTime

--- angular/src/app/modules/mytimesheet/create-edit-timesheet-item/create-edit-timesheet-item.component.ts
+++ angular/src/app/modules/mytimesheet/create-edit-timesheet-item/create-edit-timesheet-item.component.ts
@@ -132,8 +132,7 @@ export class CreateEditTimesheetItemComponent extends AppComponentBase implement
 
   private processSpecialTask(){
     if (this.isSpecialTask()){
-      // default Open Talk to 1h instead of 4h
-      this.strWorkingTime = 1;
+      this.strWorkingTime = 4;
     }
   }
 
@@ -165,8 +164,7 @@ export class CreateEditTimesheetItemComponent extends AppComponentBase implement
 
   onTaskChange(): void {
     if (this.isSpecialTask()) {
-      // default Open Talk to 1h instead of 4h
-      this.strWorkingTime = 1;
+      this.strWorkingTime = 4;
       this.myTimesheet.typeOfWork = APP_CONFIG.EnumTypeOfWork[0].value;
     }
 
