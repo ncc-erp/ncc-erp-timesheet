854be9486d6f051d68d274c3c47147a23a5a092a
tranducduong1402 <tranducduong14022001@gmail.com>
2025-06-04 18:06:11 +0700

    feat: login with mezonId

---
+++ aspnet-core/src/Timesheet.EntityFrameworkCore/Migrations/20250604104649_add_mezonId_to_user_table.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Timesheet.Migrations
+{
+    public partial class add_mezonId_to_user_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MezonId",
+                table: "AbpUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MezonId",
+                table: "AbpUsers");
+        }
+    }
+}
