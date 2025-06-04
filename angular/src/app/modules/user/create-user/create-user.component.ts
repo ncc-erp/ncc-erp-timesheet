90bb17bb5271ee04e7524bebed35ec5497b33ba4
phuongtrinhviet <phuong.trinhviet@ncc.asia>
2025-06-04 14:24:28 +0700

    feature/add MezonId field, DTOs and update create/update forms

--- angular/src/app/modules/user/create-user/create-user.component.ts
+++ angular/src/app/modules/user/create-user/create-user.component.ts
@@ -135,6 +135,7 @@ export class CreateUserComponent extends AppComponentBase implements OnInit {
         this.user.allowedLeaveDay = 0;
         this.user.branchId = this.appSession.user.branchId != null ? this.appSession.user.branchId : 1;
         this.managersFiltered = this.managers.slice();
+        this.user.mezonId = '';
         this.changeTime()
         this.userService.getRoles().subscribe(data => {
           this.roles = data.result.items;
@@ -438,7 +439,7 @@ export class createUserDTO {
   type: number;
   jobTitle: string;
   level: number;
-  isWorkingTimeDefault : boolean;
+  isWorkingTimeDefault: boolean;
   registerWorkDay: string;
   allowedLeaveDay: number;
   startDateAt: any;
@@ -457,8 +458,9 @@ export class createUserDTO {
   isStopWork: boolean;
   branchId: number;
   positionId: number;
-  beginLevel:number;
-  endDateAt:any;
+  beginLevel: number;
+  endDateAt: any;
+  mezonId: string;
 }
 
 
