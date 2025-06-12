fc4e28c75d4a9ec45234e3f8c6ef15b3021561a7
phuongtrinhviet <phuong.trinhviet@ncc.asia>
2025-06-12 13:42:05 +0700

    add table user punishment and punishment system and migration

---
+++ aspnet-core/src/Timesheet.Core/Entities/PunishmentSystem.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Timesheet.Entities
+{
+    public class PunishmentSystem : FullAuditedEntity<long>
+    {
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; } 
+
+        public int Money { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ aspnet-core/src/Timesheet.Core/Entities/UserPunishment.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Ncc.Authorization.Users;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Timesheet.Entities
+{
+    public class UserPunishment : FullAuditedEntity<long>
+    {
+        public DateTime DateAt { get; set; }
+        public long? UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+        [ForeignKey(nameof(PunishmentSystemId))]
+        public PunishmentSystem PunishmentSystem { get; set; }
+        public long PunishmentSystemId { get; set; }
+        public string Type { get; set; } // ANT, UnlockTS, ...
+        public int Count { get; set; }
+        public int TotalMoney { get; set; }
+        [MaxLength(1000)]
+        public string UserNote { get; set; }
+        [MaxLength(1000)]
+        public string NoteReply { get; set; }
+    }
+}
