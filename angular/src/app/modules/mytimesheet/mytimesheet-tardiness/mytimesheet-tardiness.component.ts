8a92d277e1688deb730a9cfbca8faf2e30c954f2
phuongtrinhviet <phuong.trinhviet@ncc.asia>
2025-07-23 15:28:19 +0700

    fix BasicUser Role for show neccessary column

--- angular/src/app/modules/mytimesheet/mytimesheet-tardiness/mytimesheet-tardiness.component.ts
+++ angular/src/app/modules/mytimesheet/mytimesheet-tardiness/mytimesheet-tardiness.component.ts
@@ -40,7 +40,6 @@ export class MytimesheetTardinessComponent extends AppComponentBase implements O
   public countLate: number = 0;
   totalMonthlyPunishment: number = 0;
   public maskTime = [/[\d]/, /\d/, ':', /\d/, /\d/];
-  isBasicUser: boolean = false;
 
   constructor(
     private timekeepingService: TimekeepingService,
