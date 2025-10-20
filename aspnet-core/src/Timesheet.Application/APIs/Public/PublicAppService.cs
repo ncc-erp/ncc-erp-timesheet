8c899b077a3b01616ae5b346b4f20bf6241fb03c
duongtd1402@gmail.com <duongtd1402@gmail.com>
2025-10-20 17:15:47 +0700

    fix: GetPMsOfUser api related to daily

--- aspnet-core/src/Timesheet.Application/APIs/Public/PublicAppService.cs
+++ aspnet-core/src/Timesheet.Application/APIs/Public/PublicAppService.cs
@@ -826,50 +826,40 @@ namespace Timesheet.APIs.Public
                 AvatarPath = x.AvatarPath
             }).ToList();
         }
+
         [HttpGet]
-        public List<PMsOfUser> GetPMsOfUser(string email, DateTime? startDate = null, DateTime? endDate = null)
+        public List<PMsOfUser> GetPMsOfUser(string email)
         {
-            using (CurrentUnitOfWork.DisableFilter(AbpDataFilters.SoftDelete))
-            {
-                IQueryable<ProjectUser> ApplyFilter(IQueryable<ProjectUser> query)
-                {
-                    if (startDate.HasValue || endDate.HasValue)
+            var qPms = WorkScope.GetAll<ProjectUser>()
+                    .Where(p => p.Type == ProjectUserType.PM)
+                    .Select(p => new
                     {
-                        query = query.Where(x => (!startDate.HasValue || x.CreationTime.Date >= startDate.Value.Date) &&
-                          (!endDate.HasValue || x.CreationTime.Date <= endDate.Value.Date));
-                    }
-                    return query;
-                }
-                var baseQuery = ApplyFilter(WorkScope.GetAll<ProjectUser>()
-                  .Where(s => s.Project.Status == ProjectStatus.Active));
-                var userProjects = baseQuery
-                  .Where(x => x.User.EmailAddress == email)
-                  .Select(x => x.ProjectId)
-                  .Distinct();
-                if (!userProjects.Any())
+                        ProjectId = p.ProjectId,
+                        UserType = p.User.Type,
+                        FullName = p.User.FullName,
+                        BranchName = p.User.Branch.Name,
+                        EmailAddress = p.User.EmailAddress,
+                        AvatarPath = p.User.AvatarPath
+                    });
+            return WorkScope.GetAll<ProjectUser>()
+                .Where(s => s.Project.Status == ProjectStatus.Active)
+                .Where(s => s.Type != ProjectUserType.DeActive)
+                .Where(x => x.User.EmailAddress == email)
+                .Select(x => new PMsOfUser()
                 {
-                    return new List<PMsOfUser>();
-                }
-                var result = baseQuery
-                  .Where(p => p.Type == ProjectUserType.PM)
-                  .Where(p => userProjects.Contains(p.ProjectId))
-                  .GroupBy(p => p.Project)
-                  .Select(g => new PMsOfUser
-                  {
-                      ProjectName = g.Key.Name,
-                      ProjectCode = g.Key.Code,
-                      PMs = g.Select(pm => new UserInfo
-                      {
-                          UserType = pm.User.Type,
-                          FullName = pm.User.FullName,
-                          BranchName = pm.User.Branch.Name,
-                          EmailAddress = pm.User.EmailAddress,
-                          AvatarPath = pm.User.AvatarPath
-                      }).ToList()
-                  })
-                  .ToList();
-                return result;
-            }
+                    ProjectName = x.Project.Name,
+                    ProjectCode = x.Project.Code,
+                    PMs = qPms
+                    .Where(p => p.ProjectId == x.ProjectId)
+                    .Select(p => new UserInfo
+                    {
+                        UserType = p.UserType,
+                        FullName = p.FullName,
+                        BranchName = p.BranchName,
+                        EmailAddress = p.EmailAddress,
+                        AvatarPath = p.AvatarPath
+                    }).ToList()
+                }).ToList();
         }
 
         [AbpAllowAnonymous]
