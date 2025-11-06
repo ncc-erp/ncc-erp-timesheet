eef13538d66e7a193ad60021c1ce0af404cb1084
HOANGMINHQUAN712004 <quan.hoangminh1@ncc.asia>
2025-11-06 15:07:36 +0700

    add new api to get working time hour of user wfh

--- aspnet-core/src/Timesheet.Application/APIs/Public/Dto/GetWorkingTimeDto.cs
+++ aspnet-core/src/Timesheet.Application/APIs/Public/Dto/GetWorkingTimeDto.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Ncc.Entities.Enum;
+using static Ncc.Entities.Enum.StatusEnum;
 namespace Timesheet.APIs.Public.Dto
 {
     public class GetWorkingTimeDto
@@ -11,5 +12,11 @@ namespace Timesheet.APIs.Public.Dto
         public string MorningEndTime { get; set; }
         public string AfternoonStartTime { get; set; }
         public string AfternoonEndTime { get; set; }
+        public DateTime DateAt { get; set; }
+        public double Hour { get; set; }
+        public RequestStatus Status { get; set; }
+        public long RequestId { get; set; }
+        public DayType DayType { get; set; }
+        public OnDayType? AbsenceTime { get; set; }
     }
 }
