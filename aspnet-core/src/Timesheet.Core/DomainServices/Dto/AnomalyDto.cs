4fcbf809eb30b5c371831f4694061a15360f4325
QuineNguyen <huynh6255@gmail.com>
2025-11-24 14:14:04 +0700

    [DRAFT - BE - STG] Improve logic of anomalies report

--- aspnet-core/src/Timesheet.Core/DomainServices/Dto/AnomalyDto.cs
+++ aspnet-core/src/Timesheet.Core/DomainServices/Dto/AnomalyDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using static Ncc.Entities.Enum.StatusEnum;
 
 namespace Timesheet.DomainServices.Dto
 {
@@ -7,19 +8,87 @@ namespace Timesheet.DomainServices.Dto
     {
         public long UserId { get; set; }
         public string EmployeeName { get; set; }
+        public string UserName { get; set; }
         public string Date { get; set; }
         public string ActualHours { get; set; }
         public string Notes { get; set; }
+        public BranchToDisplayDto Branch { get; set; }
     }
 
     public class LastWeekAnomalyDTO
     {
         public long UserId { get; set; }
         public string EmployeeName { get; set; }
+        public string UserName { get; set; }
         public List<string> DatesMissed { get; set; } = new List<string>();
         public List<string> DatesNoTrackerTime { get; set; } = new List<string>();
         public List<string> DatesBelowThreshold { get; set; } = new List<string>();
         public int Count { get; set; }
         public string Notes { get; set; }
+        public BranchToDisplayDto Branch { get; set; }
+    }
+
+    public class TimekeepingDto
+    {
+        public long? UserId { get; set; }
+        public DateTime DateAt { get; set; }
+        public string CheckIn { get; set; }
+        public string CheckOut { get; set; }
+        public string TrackerTime { get; set; }
+    }
+
+    public class AbsenceRequestDto
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public RequestStatus Status { get; set; }
+        public RequestType Type { get; set; }
+    }
+    public class AbsenceDetailDto
+    {
+        public long RequestId { get; set; }
+        public DateTime DateAt { get; set; }
+        public DayType DateType { get; set; }
+        public OnDayType? AbsenceTime { get; set; }
+        public double Hour { get; set; }
+        public RequestStatus RequestStatus { get; set; }
+        public RequestType RequestType { get; set; }
+        public long UserId { get; set; }
+    }
+    public class AnomalyData
+    {
+        public List<UserDto> Users { get; set; } = new List<UserDto>();
+        public List<TimekeepingDto> Timekeepings { get; set; } = new List<TimekeepingDto>();
+        public List<AbsenceRequestDto> AbsenceRequests { get; set; } = new List<AbsenceRequestDto>();
+        public List<AbsenceDetailDto> AbsenceDetails { get; set; } = new List<AbsenceDetailDto>();
+    }
+    public class BranchToDisplayDto
+    {
+        public string BranchName { get; set; }
+        public string BranchColor { get; set; }
+    }
+    public class AnomalyInputDto
+    {
+        public long UserId { get; set; }
+        public string EmployeeName { get; set; }
+        public string UserName { get; set; }
+        public string Date { get; set; }
+        public double? TotalWorkingTime { get; set; }
+        public string Notes { get; set; }
+        public bool IsYesterdayInFunction { get; set; }
+        public string ViolationType { get; set; } = "DatesBelowThreshold";
+        public BranchToDisplayDto Branch { get; set; }
+    }
+    public class ProcessAnomaliesInputDto
+    {
+        public List<UserDto> AllUsers { get; set; }
+        public List<TimekeepingDto> AllTimekeepings { get; set; }
+        public List<AbsenceRequestDto> AllAbsenceRequests { get; set; }
+        public List<AbsenceDetailDto> AllAbsenceDetails { get; set; }
+        public long BranchId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsYesterday { get; set; }
+        public BranchToDisplayDto Branch { get; set; }
     }
 }
