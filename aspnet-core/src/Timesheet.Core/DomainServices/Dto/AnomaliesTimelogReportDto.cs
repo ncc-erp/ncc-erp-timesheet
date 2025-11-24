2a50a8a661009ba5d9764eb5866d694991fa2f59
QuineNguyen <huynh6255@gmail.com>
2025-11-24 14:14:04 +0700

    [DRAFT - BE - STG] Improve logic of anomalies report

---
+++ aspnet-core/src/Timesheet.Core/DomainServices/Dto/AnomaliesTimelogReportDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Timesheet.DomainServices.Dto;
+
+namespace Timesheet.DomainServices.Dto
+{
+    public class AnomaliesTimelogReportDto
+    {
+        public string Yesterday { get; set; }
+        public string LastWeekStart { get; set; }
+        public string LastWeekEnd { get; set; }
+        public List<YesterdayAnomalyDTO> YesterdayAnomalies { get; set; }
+        public List<LastWeekAnomalyDTO> LastWeekAnomalies { get; set; }
+
+        public AnomaliesTimelogReportDto()
+        {
+            YesterdayAnomalies = new List<YesterdayAnomalyDTO>();
+            LastWeekAnomalies = new List<LastWeekAnomalyDTO>();
+        }
+    }
+
+    public class GetAnomaliesTimelogReportInput
+    {
+        public List<long> BranchIds { get; set; } = new List<long>();
+    }
+}
