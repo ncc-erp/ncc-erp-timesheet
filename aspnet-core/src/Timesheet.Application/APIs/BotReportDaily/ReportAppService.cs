d073b7f5cece5cc711996059af079699024731a5
QuineNguyen <huynh6255@gmail.com>
2025-11-25 14:16:34 +0700

    update not including office working service and anomalies service

--- aspnet-core/src/Timesheet.Application/APIs/BotReportDaily/ReportAppService.cs
+++ aspnet-core/src/Timesheet.Application/APIs/BotReportDaily/ReportAppService.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Timesheet.APIs.Reports;
-using Timesheet.APIs.BotReportDaily.Dto;
 using Timesheet.DomainServices;
 using Timesheet.DomainServices.Dto;
 using Timesheet.Services.Mezon;
@@ -21,32 +20,14 @@ namespace Timesheet.APIs.BotReportDaily
         private readonly IWorkScope _workScope;
         private readonly MezonService _mezonService;
         private readonly ISettingManager _settingManager;
-        private readonly IAbsenceDayServices _absenceDayService;
-        private readonly OfficeWorkingReportAppService _officeWorkingReportAppService;
         private readonly IBotReportDailyService _botReportDailyService;
 
-        public ReportAppService(IWorkScope workScope, MezonService mezonService, ISettingManager settingManager, DomainServices.AbsenceDayServices absenceDayService, OfficeWorkingReportAppService officeWorkingReportAppService, DomainServices.BotReportDailyService botReportDailyService)
+        public ReportAppService(IWorkScope workScope, MezonService mezonService, ISettingManager settingManager, DomainServices.BotReportDailyService botReportDailyService)
         {
             _workScope = workScope;
             _mezonService = mezonService;
             _settingManager = settingManager;
-            _absenceDayService = absenceDayService;
-            _officeWorkingReportAppService = officeWorkingReportAppService;
             _botReportDailyService = botReportDailyService;
-        }
-
-        [HttpGet]
-        [AbpAuthorize(Ncc.Authorization.PermissionNames.BranchDirector_Report, Ncc.Authorization.PermissionNames.BranchDirector_AnomaliesReport_View)]
-        public async Task<AnomaliesTimelogReportDto> GetAnomaliesTimelogReport(GetAnomaliesTimelogReportInput input)
-        {
-            return await _absenceDayService.GetAnomaliesTimelogReport(input);
-        }
-
-        [HttpGet]
-        [AbpAuthorize(Ncc.Authorization.PermissionNames.BranchDirector_Report, Ncc.Authorization.PermissionNames.BranchDirector_OfficeWorkingReport_View)]
-        public async Task<OfficeWorkingTimelogReportDto> GetOfficeWorkingTimelogReport(GetOfficeWorkingTimelogReportInput input)
-        {
-            return await _officeWorkingReportAppService.GetOfficeWorkingTimelogReport(input);
         }
 
         [HttpGet]
