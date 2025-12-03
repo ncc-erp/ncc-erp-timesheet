c4781318310d6caebd1b2edddec9310c6f5e32b4
HOANGMINHQUAN712004 <quan.hoangminh1@ncc.asia>
2025-12-03 13:47:16 +0700

    update deactive user logic

--- aspnet-core/src/Timesheet.Application/APIs/Timesheets/Projects/ProjectAppService.cs
+++ aspnet-core/src/Timesheet.Application/APIs/Timesheets/Projects/ProjectAppService.cs
@@ -868,12 +868,16 @@ namespace Timesheet.Timesheets.Projects
             }
             if (projectUser.Type == ProjectUserType.PM)
             {
-                throw new UserFriendlyException("You can't deactive PM of this project!");
+                var otherPMsCount = WorkScope.GetAll<ProjectUser>()
+                    .Count(pu => pu.Project.Id == projectId 
+                       && pu.User.Id != userId 
+                       && pu.Type == ProjectUserType.PM);
+                if (otherPMsCount == 0)
+                {
+                    throw new UserFriendlyException("Cannot deactivate the only PM in the project. Please assign another PM first.");
+                }
             }
-            else
-            {
-                projectUser.Type = ProjectUserType.DeActive;
-            }
+            
             projectUser.Type = ProjectUserType.DeActive;
             await WorkScope.UpdateAsync(projectUser);
         }
