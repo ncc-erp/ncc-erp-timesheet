fc18c0e11f2aeb3ed68302206861f77bf539fd6c
BinhMinhPTIT <binhminhtt03@gmail.com>
2025-12-17 13:32:17 +0700

    fix: Auto-login from mini app

--- angular/src/account/login/login.component.ts
+++ angular/src/account/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, Injector, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AbpSessionService } from '@abp/session/abp-session.service';
 import { AppComponentBase } from '@shared/app-component-base';
 import { accountModuleAnimation } from '@shared/animations/routerTransition';
@@ -7,6 +8,8 @@ import { AuthService, SocialUser } from "angularx-social-login";
 import { GoogleLoginProvider } from "angularx-social-login";
 import { AppConsts } from '@shared/AppConsts';
 import { MezonLoginService } from '@app/service/api/mezon-api.service';
+import { IHashMezonAuthModel } from '@shared/service-proxies/service-proxies';
+import { Base64 } from 'js-base64';
 
 
 @Component({
@@ -29,6 +32,7 @@ export class LoginComponent extends AppComponentBase implements OnInit {
     private _sessionService: AbpSessionService,
     private authService: AuthService,
     private mezonLoginService: MezonLoginService,
+    private route: ActivatedRoute,
 
   ) {
     super(injector);
@@ -42,6 +46,20 @@ export class LoginComponent extends AppComponentBase implements OnInit {
     // Disable Auto Login By Google
     this.authService.authState.subscribe((user) => {
     }, err => this.authService.signOut());
+
+    this.route.queryParams.subscribe(params => {
+      const hashFromUrl = params['data'];
+      if (hashFromUrl) {
+        this.isMezonApp = true;
+        this.hashData = hashFromUrl;
+        const hashAuthData: IHashMezonAuthModel = {
+          hashData: Base64.encode(hashFromUrl),
+        };
+        this.loginService.authenticateMezonHash(hashAuthData, () => {
+          this.isMezonApp = true;
+        });
+      }
+    });
   }
 
 
