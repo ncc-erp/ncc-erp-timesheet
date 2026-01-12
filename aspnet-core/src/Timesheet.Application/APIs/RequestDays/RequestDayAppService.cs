721cb5dc8e9588bae6d1bfbb8cff39961d596993
QuineNguyen <huynh6255@gmail.com>
2026-01-12 10:31:24 +0700

    fix: branch director cannot approve request when user is not a member of project which branch director is PM

--- aspnet-core/src/Timesheet.Application/APIs/RequestDays/RequestDayAppService.cs
+++ aspnet-core/src/Timesheet.Application/APIs/RequestDays/RequestDayAppService.cs
@@ -1323,11 +1323,11 @@ namespace Timesheet.APIs.RequestDays
                     throw new UserFriendlyException("You cannot approve your own request!");
                 }
 
-				if (!(await CheckSessionUserIsPMOfUser(request.UserId)))
-				{
+				if (isViewBranch == false && !(await CheckSessionUserIsPMOfUser(request.UserId)))
+                {
 					throw new UserFriendlyException("You are not PM of UserId " + request.UserId);
 				}
-                else if (isViewBranch == true || (await CheckSessionUserIsPMOfUser(request.UserId)))
+                else
                 {
                     var requestDetails = await WorkScope.GetAll<AbsenceDayDetail>()
                         .Where(s => s.RequestId == requestId)
