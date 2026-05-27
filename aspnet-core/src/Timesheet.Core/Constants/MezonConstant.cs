4a990ff956ea4c40b312383fa13c94f0c22e104a
QuineNguyen <huynh6255@gmail.com>
2026-05-27 17:58:38 +0700

    add bold markup when sending message to channel

--- aspnet-core/src/Timesheet.Core/Constants/MezonConstant.cs
+++ aspnet-core/src/Timesheet.Core/Constants/MezonConstant.cs
@@ -9,8 +9,10 @@ namespace Timesheet.Constants
         public const string MEZON_NOTI_TYPE = "TIMESHEET";
         public const string MULTILINE_MARKDOWN_CODE_TYPE = "t";
         public const string INLINE_MARKDOWN_CODE_TYPE = "s";
+        public const string BOLD_TYPE = "b";
         public const string TRIPLE_BACKTICKS = "```";
         public const string MARKDOWN_PATTERN = @"(```|`)";
         public const string MENTION_PATTERN = @"@([a-zA-Z0-9.]+)";
+        public const string BOLD_MARKER = "**";
     }
 }
